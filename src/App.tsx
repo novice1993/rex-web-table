c804b2a6d3042ebb0cee5dbfa8d076c24ff02544
kimyh93 <kimyh93@rexgen.co.kr>
2024-09-06 12:03:46 +0900

    feat: TablePagination 컴포넌트 구현
    Desc
    - table pagination과 관련된 ui 컴포넌트 구현
    - setState 함수를 props로 전달받아서 page index 변경하는 로직 포함

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import {
 import { Table } from "@mantine/core";
 import TableHeader from "./components/TableHeader";
 import TableBody from "./components/TableBody";
-import { Pagination } from "@mantine/core";
+import TablePagination from "./components/TablePagination";
+
 import { Select } from "@mantine/core";
 
 import { data, columns, headerOptionType } from "./dummyData";
 
 import useTablePagination from "./hook/useTablePagination";
-import { useEffect } from "react";
 
 export interface Example {
   No: number;
@@ -40,30 +40,13 @@ function App() {
     state: { pagination },
   });
 
-  useEffect(() => console.log("%c page 관련 info", "color: red", pagination));
-
-  // page index event handler
-  const handleChangePageIndex = (pageNum: number) => {
-    // page number에서 -1을 한 값이 data와 연결되는 pageIndex와 일치하므로 -1 처리
-    const newPageIndex = pageNum - 1;
-
-    setPagination((prevState: PaginationState) => {
-      return { pageSize: prevState.pageSize, pageIndex: newPageIndex };
-    });
-  };
-
   // page size event handler
   const handleChangePageSize = (pageSize: string | null) => {
     setPagination((prevState: PaginationState) => {
       // page size 변경에 맞춰 page number 조절
       const currentItemIndex = prevState.pageIndex * prevState.pageSize;
-      console.log("%c 현재 아이템 index", "color: yellow", currentItemIndex);
-
       const newPageSize = Number(pageSize);
-      console.log("%c 현재 페이지 size", "color: yellow", newPageSize);
-
       const newPageNum = Math.ceil(currentItemIndex / newPageSize);
-      console.log("%c 현재 페이지 number", "color: yellow", newPageNum);
 
       // page number에서 -1을 한 값이 data와 연결되는 pageIndex와 일치하므로 -1 처리 (단, 0일 경우 제외)
       const newPageIndex = newPageNum > 0 ? newPageNum - 1 : newPageNum;
@@ -91,10 +74,10 @@ function App() {
           value={String(pagination.pageSize)}
           onChange={handleChangePageSize}
         />
-        <Pagination
-          total={totalPageNum}
-          value={pagination.pageIndex + 1}
-          onChange={handleChangePageIndex}
+        <TablePagination
+          totalPageNum={totalPageNum}
+          pagination={pagination}
+          setPagination={setPagination}
         />
       </div>
     </>
---
+++ src/components/TablePagination.tsx
@@ -0,0 +1,32 @@
+import { Pagination } from "@mantine/core";
+import { PaginationState } from "@tanstack/react-table";
+import { Dispatch, SetStateAction } from "react";
+
+interface TablePaginationProps {
+  totalPageNum: number;
+  pagination: PaginationState;
+  setPagination: Dispatch<SetStateAction<PaginationState>>;
+}
+
+const TablePagination = (props: TablePaginationProps) => {
+  const { totalPageNum, pagination, setPagination } = props;
+
+  const handleChangePageIndex = (pageNum: number) => {
+    // page number에서 -1을 한 값이 data와 연결되는 pageIndex와 일치하므로 -1 처리
+    const newPageIndex = pageNum - 1;
+
+    setPagination((prevState: PaginationState) => {
+      return { pageIndex: newPageIndex, pageSize: prevState.pageSize };
+    });
+  };
+
+  return (
+    <Pagination
+      total={totalPageNum}
+      value={pagination.pageIndex + 1}
+      onChange={handleChangePageIndex}
+    />
+  );
+};
+
+export default TablePagination;
