790379bdec26dbf5602c7e658ed2924d7eceef40
kimyh93 <kimyh93@rexgen.co.kr>
2024-09-13 18:43:41 +0900

    [chore] 테스트 데이터 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,8 @@ import AddSubRow from "./components/SubRowComponents/AddSubRow";
 import { data, headerOptionType } from "./dummyData";
 import { ColumnDef } from "@tanstack/react-table";
 
-export const columns: ColumnDef<Example>[] = [
-  {
-    accessorKey: "No",
-    header: "No",
-    enableSorting: false,
-  },
-  {
-    accessorKey: "firstName",
-    header: "First Name",
-    enableSorting: false,
-    cell: ({ getValue }) => {
-      const cellValue = getValue() as string;
-
-      return (
-        <div
-          onClick={(e) => {
-            e.stopPropagation();
-            console.log(getValue());
-          }}
-        >
-          {cellValue}
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "add",
-    header: "add",
-    enableSorting: false,
-  },
-];
+import { changeTableCellValue } from "./util/body.util";
+import { Row } from "@tanstack/react-table";
 
 export interface Example {
   No: number;
@@ -46,6 +17,67 @@ export interface Example {
 }
 
 function App() {
+  const columns: ColumnDef<Example>[] = [
+    {
+      accessorKey: "No",
+      header: "No",
+      enableSorting: false,
+    },
+    {
+      accessorKey: "firstName",
+      header: "First Name",
+      enableSorting: false,
+    },
+    {
+      accessorKey: "add",
+      header: "add",
+      enableSorting: false,
+      cell: ({ getValue, cell, table }) => {
+        const cellValue = getValue() as string;
+
+        const addTableSubRow = () => {
+          if (cell.column.id === "add") {
+            const row = cell.row;
+            const cellValue = cell.getValue();
+
+            if (cellValue === "-") {
+              const updatedRow = subRowContent.filter((content: unknown) => {
+                const typedContent = content as Row<unknown>;
+                return typedContent.index !== row.index;
+              });
+
+              return setSubRowContent(updatedRow);
+            } else {
+              const updatedRow = changeTableCellValue(row, table, "No", "");
+              const reUpdatedRow = changeTableCellValue(
+                updatedRow,
+                table,
+                "add",
+                "-"
+              );
+              reUpdatedRow.index = subRowContent.length;
+              const newSubRowContent = [...subRowContent, reUpdatedRow];
+
+              setSubRowContent(newSubRowContent);
+            }
+          }
+        };
+
+        return (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              // console.log(getValue());
+              addTableSubRow();
+            }}
+          >
+            {cellValue}
+          </div>
+        );
+      },
+    },
+  ];
+
   const { table, subRowContent, setSubRowContent } = useTableManager<Example>({
     data,
     columns,
