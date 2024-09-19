7ea4db69d4207c11df22cf85fd62de5ef8b72b69
kimyh93 <kimyh93@rexgen.co.kr>
2024-09-19 18:38:03 +0900

    [chore] table 테스트 위해 server api 구현하여 적용 (추가 구현 필요)

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,35 @@ import TableHeader from "./components/TableHeader/index";
 import TableBody from "./components/TableBody/index";
 import TableFooter from "./components/TableFooter";
 import AddSubRow from "./components/SubRowComponents/AddSubRow";
-import { data, headerOptionType } from "./dummyData";
+import { headerOptionType } from "./dummyData";
 import { ColumnDef, Row } from "@tanstack/react-table";
 
-// test
+import { useLayoutEffect, useState } from "react";
 import { useSubRowContent } from "./hook/useSubRowContent";
 
-/**
- * 구현 필요한 부분
- *  1) row click event setting 기능 -> ok
- *  2) column/data setting
- *     sub row content setting
- *      -> 설정 관련해서 정리
- *  3) flow chart 정리
- */
+// const subRowCellClickEvent = ({
+//   cellIndex,
+//   rowIndex,
+//   e,
+// }: {
+//   cellIndex: number;
+//   rowIndex?: number;
+//   e?: React.MouseEvent<HTMLTableCellElement>;
+// }) => {
+//   if (cellIndex !== 2) return;
+
+//   if (rowIndex !== undefined) {
+//     e?.stopPropagation();
+
+//     const prevSubRowContent = getSubRowContentOfSelected(rowIndex);
+//     const testState = [...prevSubRowContent];
+
+//     if (testState.length !== 0) {
+//       testState.pop();
+//       setSubRowContenttOfSelected(rowIndex, testState);
+//     }
+//   }
+// };
 
 export interface Example {
   No: number;
@@ -71,8 +86,10 @@ function App() {
     },
   ];
 
+  // data
+  const [data, setData] = useState([]);
   const { table, totalPageNum, pagination, setPagination } = useTable<Example>({
-    data,
+    data: data,
     columns,
     isPagination: true,
   });
@@ -81,33 +98,19 @@ function App() {
     row.toggleExpanded();
   };
 
-  const subRowCellClickEvent = ({
-    cellIndex,
-    rowIndex,
-    e,
-  }: {
-    cellIndex: number;
-    rowIndex?: number;
-    e?: React.MouseEvent<HTMLTableCellElement>;
-  }) => {
-    if (cellIndex !== 2) return;
+  useLayoutEffect(function setTableData() {
+    const apiUrl = "http://localhost:8080/table/type/1";
 
-    if (rowIndex !== undefined) {
-      e?.stopPropagation();
+    const getdata = async () => {
+      const result = await fetch(apiUrl, {
+        method: "GET",
+      });
+      const data = await result.json();
+      if (data) setData(data);
+    };
 
-      const prevSubRowContent = getSubRowContentOfSelected(rowIndex);
-      const testState = [...prevSubRowContent];
-
-      if (testState.length !== 0) {
-        testState.pop();
-        setSubRowContenttOfSelected(rowIndex, testState);
-      }
-    }
-  };
-
-  const subRowClickEvent = () => {
-    alert("test func!!");
-  };
+    getdata();
+  }, []);
 
   return (
     <>
@@ -115,8 +118,8 @@ function App() {
         SubRowComponent={AddSubRow}
         useParentRowUi={true}
         rowClickEvent={rowClickEvent}
-        subRowClickEvent={subRowClickEvent}
-        subRowCellClickEvent={subRowCellClickEvent}
+        // subRowClickEvent={subRowClickEvent}
+        // subRowCellClickEvent={subRowCellClickEvent}
       >
         <TableHeader table={table} headerOptionType={headerOptionType} />
         <TableBody table={table} />
